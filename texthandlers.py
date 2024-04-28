deb09362f20123f1d164a81ef2cbec3afc853242
Watermelon-1234 <948794harvey@gmail.com>
2024-04-29 00:33:48 +0800

    build_baisc_construct-4

--- texthandlers.py
+++ texthandlers.py
@@ -1,17 +1,16 @@
 def test():
     print("just test")
+    
+class Reply:
+    def __init__(self, message="just fuck off!"):
+        self.content = message
+        self.status = 0  # 預設狀態為 0
+
 
 def roll_call(input):#名字 課程ID 是否會遲到(0/1) 備註(選填)
 
-    reply=''
+    reply = Reply()
     reply.status=1
     reply.content="just fuck off!"
     return reply
 
-def score_register(input):#課程名稱
-    
-
-    reply=''
-    reply.status=1
-    reply.content="just fuck off!"
-    return reply
