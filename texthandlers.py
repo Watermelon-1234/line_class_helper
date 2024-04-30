47f56a4c3d23e2ef337208012499ad451fbbb4d9
Watermelon-1234 <948794harvey@gmail.com>
2024-04-30 08:52:39 +0800

    build_baisc_construct-6

--- texthandlers.py
+++ texthandlers.py
@@ -2,8 +2,8 @@ def test():
     print("just test")
     
 class Reply:
-    def __init__(self, message="just fuck off!"):
-        self.content = message
+    def __init__(self, message_temp="just fuck off!"):#warning:can't use message as a argument name as it has already be initialized in the app.py?
+        self.content = message_temp
         self.status = 0  # 預設狀態為 0
 
 
